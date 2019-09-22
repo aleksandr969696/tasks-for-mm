fcad42b17fd5f994a9ff7e0ea04af4f360905573
= <aleksandr969696@mail.ru>
2019-09-22 21:48:39 +0300

    task is done

--- task1.py
+++ task1.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import time
 
+
 def nmax_of_lists(*lists, N=1):
     """
     Function takes lists and return N max values of them
@@ -17,23 +18,53 @@ def nmax_of_lists(*lists, N=1):
     union_list.sort(reverse=True)
     return union_list[:N]
 
+
 def time_count(K, M, N):
+    """
+    Function takes three int params: K, M, N. K times calls function nmax_of_lists
+    with two random 2M length lists and N, then it count time of each call and return its average.
+
+    :param K:
+    :param M:
+    :param N:
+    :return:
+    """
     times = []
     for k in range(K):
         l1 = [random.randint(0, 2*M) for _ in range(M)]
         l2 = [random.randint(0, 2*M) for _ in range(M)]
         time0 = time.time()
         nmax_of_lists(l1, l2, N=N)
-        times.append(round(time.time()-time0, 10))
-    print(times)
+        times.append(time.time()-time0)
     return np.average(times)
 
 
-print(time_count(10, 10000, 5))
-#
-# print(nmax_of_lists.__doc__)
-# l1 = [i for i in range(20) if i%2 == 0]
-# l1.append(18)
-# l2 = [i for i in range(20) if i%2 != 0]
-# l1.append(18)
-# print(nmax_of_lists(l1, l2, N = 7, numpy_=True))
+def count_of_unique(l):
+    return len(set(l))
+
+
+def count_of_unique2(l):
+    a = set()
+    count=0
+    for i in l:
+        if i not in a:
+            a.add(i)
+            count += 1
+    return count
+
+
+def count_of_unique3(l):
+    return len(np.unique(l))
+
+
+m_and_n = dict()
+
+m_and_n[1] = (1, 2, 5)
+m_and_n[10] = (2, 5, 10)
+m_and_n[1000] = (5, 50, 100)
+m_and_n[1000000] = (5, 100, 1000)
+
+with open('results_ЕфимовАА.txt', 'w') as f:
+    for M, N in m_and_n.items():
+        for n in N:
+            f.write(f'N = {n}, M = {M}, time = {round(time_count(10, M, n),3)}s\n')
